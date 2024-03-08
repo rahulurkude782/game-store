b0bd2060e749437e1a15e60c92aa1c5647415c71
rahulurkude782 <rahulurkude782@gmail.com>
2024-03-08 16:42:44 +0530

    Genre List Highlighting added

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,10 +4,11 @@ import getCropedImage from "../services/image-crop";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
+  selectedGenre: Genre | null;
   onSelectGenre: (genre: Genre) => void;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data: genre, loading, error } = useGenre();
   if (loading) return <GenreListSkeleton count={10} />;
 
@@ -24,7 +25,11 @@ const GenreList = ({ onSelectGenre }: Props) => {
               borderRadius={4}
               objectFit="cover"
             />
-            <Button onClick={() => onSelectGenre(genre)} variant="link">
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+            >
               {genre.name}
             </Button>
           </HStack>
