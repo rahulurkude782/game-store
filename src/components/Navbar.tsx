30a1e63bc30ee3f4189f6a9693482d2ad1d0622f
rahulurkude782 <rahulurkude782@gmail.com>
2024-03-27 15:03:08 +0530

    Add Link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,15 @@ import logo from "../assets/logo7.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import useGameStore from "../store/useGameStore";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const setSearchText = useGameStore((store) => store.setSearchText);
   return (
     <HStack paddingX="20px" paddingY="10px" shadow={"md"} marginBottom="15px">
-      <Image src={logo} boxSize="60px" objectFit="cover" borderRadius="md" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" borderRadius="md" />
+      </Link>
       <Box width="100%" paddingX="15px">
         <SearchInput onSearch={(searchString) => setSearchText(searchString)} />
       </Box>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   onSearch: (searchString: string) => void;
@@ -8,11 +9,15 @@ interface Props {
 
 const SearchInput = ({ onSearch }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (inputRef.current) onSearch(inputRef.current.value);
+        navigate("/");
+        //@ts-expect-error it might be null
+        inputRef.current.value = "";
       }}
     >
       <InputGroup>
