2684c3f1afeec65df65109458917d34815ffa961
rahulurkude782 <rahulurkude782@gmail.com>
2024-03-27 11:26:50 +0530

    Implement Expandable Text Component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,36 @@
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+
+const ExpandableText = ({ game }: Props) => {
+  const [isExpandable, setExpandable] = useState(false);
+  const length = 300;
+  const string = isExpandable
+    ? game.description_raw
+    : game.description_raw.substring(0, length);
+
+  const description =
+    game.description_raw.length <= length ? game.description_raw : string;
+
+  return (
+    <Box>
+      <Heading>{game.name}</Heading>
+      <Text>
+        {description}...
+        <Button
+          marginX="10px"
+          colorScheme="yellow"
+          onClick={() => setExpandable(!isExpandable)}
+        >
+          {isExpandable ? "Show less" : "Read more"}
+        </Button>
+      </Text>
+    </Box>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,16 +1,21 @@
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, HStack, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const params = useParams();
   const { data: game, isPending, error } = useGame(params.slug!);
-  if (isPending) return <Spinner />;
+  if (isPending)
+    return (
+      <HStack alignItems="center" justifyContent="center" height="80vh">
+        <Spinner />
+      </HStack>
+    );
   if (error || !game) throw error;
   return (
     <Box padding={5}>
-      <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText game={game} />
     </Box>
   );
 };
